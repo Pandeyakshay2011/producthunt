3622d844833a28b8d98be31879164c4eba79f117
Pandeyakshay2011 <35912160+Pandeyakshay2011@users.noreply.github.com>
2018-12-03 19:39:19 +0530

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    url = models.TextField()
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub.strftime('%b %e %Y')
